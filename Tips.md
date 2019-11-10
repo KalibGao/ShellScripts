567e2ab88e680a72f557c6d1b82ad3adf335a16d
kalib.gao <gaoziyin@vip.163.com>
2019-11-10 18:07:08 +0800

    add nginx auto installation

---
+++ Tips.md
@@ -0,0 +1,6 @@
+# ShellScripts Experiment
+
+## sh 脚本
+- 脚本要求无用户中断, 例如 `yum update -y ...`, `yum install -y ...`
+
+## 
--- linux-update.sh
+++ linux-update.sh
@@ -15,4 +15,30 @@ sudo yum-config-manager \
 
 sudo yum install -y docker-ce docker-ce-cli containerd.io 
 
+sudo systemctl enable docker  | sudo systemctl restart docker
+
 # install nginx
+sudo yum install -y yum-utils
+
+sudo tee /etc/yum.repos.d/nginx.repo <<- 'EOF'
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+EOF
+
+sudo yum install -y nginx
+
+sudo systemctl enable nginx | sudo systemctl restart nginx
+

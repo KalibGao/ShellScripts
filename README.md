5199c235065080e5e90cd258ce51800998fa3af8
kalib.gao <gaoziyin@vip.163.com>
2019-11-11 13:28:08 +0800

    add docker-compose installation guide

--- README.md
+++ README.md
@@ -3,7 +3,22 @@ Commonly used shell scripts for daily work.
 
 ## Simple usage
 
-- `curl` download and shell execute
+`curl` download and shell execute
+
+- centos 7 system update 
+
 ```
-curl -sSL https://raw.githubusercontent.com/KalibGao/ShellScripts/master/linux-update.sh | sh 
+curl -sSL https://raw.githubusercontent.com/KalibGao/ShellScripts/master/linux-update.sh | sudo sh 
 ```
+
+- install git 
+
+```
+curl -sSL https://raw.githubusercontent.com/KalibGao/ShellScripts/master/git-install.sh | sudo sh
+```
+
+- install docker and docker-compose
+```
+curl -sSL https://raw.githubusercontent.com/KalibGao/ShellScripts/master/docker-install.sh | sudo sh
+```
+
--- docker-install.sh
+++ docker-install.sh
@@ -10,3 +10,17 @@ sudo yum-config-manager \
 sudo yum install -y docker-ce docker-ce-cli containerd.io 
 
 sudo systemctl enable docker  | sudo systemctl restart docker
+
+
+# install docker-compose
+ 
+ # get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Output compose version
+docker-compose -v

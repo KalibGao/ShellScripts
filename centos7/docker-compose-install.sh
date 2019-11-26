15e855952f0a3573ebcc7a965b988f9df065b7d6
kalib.gao <gaoziyin@vip.163.com>
2019-11-26 22:31:43 +0800

    add docker-compose mirror

--- centos7/docker-compose-install.sh
+++ centos7/docker-compose-install.sh
@@ -3,8 +3,11 @@
 # get latest docker compose released tag
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
-# Install docker-compose
-sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-chmod +x /usr/local/bin/docker-compose
-sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+# # Install docker-compose
+# sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+# chmod +x /usr/local/bin/docker-compose
+# sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
+# use mirror
+curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
